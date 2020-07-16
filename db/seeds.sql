4db18903998054e368553ee59b160ec522e5c537
Leonard Esere <leonardesere@gmail.com>
2020-07-16 13:04:34 -0500

    updated and created all code elements

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burger (burger_name, devoured) VALUES ('BBQ BACON CHEESEBURGER', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('JUMBO JACK', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('OLD TIMER', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('SPICY JALAPENO BAJA', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('KRUSTY BURGER', true);
